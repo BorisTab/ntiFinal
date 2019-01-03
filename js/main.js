de016bd9e1cd2eeb5c54de2cbc58848185d619ff
BorisTab <boristab23@gmail.com>
2019-01-03 14:25:13 +0300

    menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 function menu(x) {
     x.classList.toggle("change");
+}
 
-    let verticalMenu = document.getElementsByClassName('vertical-menu');
+function show(){
+    let verticalMenu = document.getElementsByClassName('vertical-menu')[0];
     if (verticalMenu.style.display === 'none'){
         verticalMenu.style.display = 'block';
-    }
-    else {
+    } else {
         verticalMenu.style.display = 'none';
     }
 }
+
