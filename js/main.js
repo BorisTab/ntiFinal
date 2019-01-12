4126ec61f3cc12750cc88e8d0f2c583d8cfe92ac
BorisTab <boristab23@gmail.com>
2019-01-12 19:53:19 +0300

    underline menu button

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ $('document').ready(function () {
         }, 800);
     });
 
-    $('.portfolio-button, .vertical-menu-portfolio').click(function () {
+    /*$('.portfolio-button, .vertical-menu-portfolio').click(function () {
         $('html, body').animate({
             scrollTop: $('#section-2').offset().top
         }, 1000);
@@ -96,7 +96,7 @@ $('document').ready(function () {
             scrollTop: $('#section-5').offset().top
         }, 1000);
         hide();
-    });
+    });*/
 
     let slideWidth = $('#section-4').innerWidth();
     let sliderTimer;
@@ -172,5 +172,46 @@ $('document').ready(function () {
     });
     $('.eng, .vertical-menu-eng').click(function () {
         window.location = 'index-eng.html';
+    });
+
+
+
+    var lastId,
+        topMenu = $('.menu'),
+        // All list items
+        menuItems = topMenu.find('a'),
+        // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function(){
+            var item = $($(this).attr("href"));
+            if (item.length) return item;
+        });
+    menuItems.click(function(e){
+        var href = $(this).attr("href"),
+            offsetTop = href === "#" ? 0 : $(href).offset().top + 1;
+        $('html, body').stop().animate({
+            scrollTop: offsetTop
+        }, 800);
+        e.preventDefault();
+    });
+    $(window).scroll(function(){
+        // Get container scroll position
+        var fromTop = $(this).scrollTop();
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function(){
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length-1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id) {
+            lastId = id;
+            // Set/remove active class
+            menuItems
+                .parent().removeClass("active-button")
+                .end().filter("[href='#"+id+"']").parent().addClass("active-button");
+        }
     });
 });
